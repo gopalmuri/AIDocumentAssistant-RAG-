9a3539cee1752854cd0a24eb313f6de71647a18a
gopalmuri <gopalmuri2004@gmail.com>
2025-12-18 11:23:48 +0530

    Fix: Add spacy model download to build.sh

--- mainfolder/build.sh
+++ mainfolder/build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 # Install dependencies
 pip install -r requirements.txt
+python -m spacy download en_core_web_sm
 
 # Convert static asset paths
 python backend/manage.py collectstatic --no-input
